09ac46b13ec82c885f2eef81063f5f2f074cff3c
adzettelUM <adzettel@umich.edu>
2025-12-11 01:13:45 -0500

    Added fireable transitions to visualizer

---
+++ UpdateablePetriSim.py
@@ -0,0 +1,155 @@
+from flask import Flask, jsonify, request, send_file
+from pyvis.network import Network
+import threading
+import webbrowser
+import os
+
+# --- Petri Net Core ---
+class PetriNet:
+    def __init__(self):
+        self.places = {"P1": 1, "P2": 0}
+        self.transitions = ["T1", "T2"]
+        self.arcs_in = {"T1": [("P1", 1)], "T2": [("P2", 1)]}
+        self.arcs_out = {"T1": [("P2", 1)], "T2": [("P1", 1)]}
+
+    def fireable(self):
+        fireable = []
+        for t, arcs in self.arcs_in.items():
+            if all(self.places[p] >= w for p, w in arcs):
+                fireable.append(t)
+        return fireable
+
+    def fire(self, transition):
+        if transition not in self.fireable():
+            return False
+        for p, w in self.arcs_in[transition]:
+            self.places[p] -= w
+        for p, w in self.arcs_out[transition]:
+            self.places[p] += w
+        return True
+
+# --- PyVis Visualizer ---
+class PetriNetVisualizer:
+    def __init__(self, pn: PetriNet):
+        self.pn = pn
+        self.net = Network(directed=True, notebook=False)
+        self.place_nodes = {}
+        self.transition_nodes = {}
+        self.built = False
+
+    def build_graph(self):
+        if self.built:
+            return
+
+        # Places
+        for place, tokens in self.pn.places.items():
+            node_id = f"p_{place}"
+            label = f"{place}\nTokens: {tokens}"
+            self.net.add_node(node_id, label=label, shape='circle', color='#89CFF0', size=30 + tokens*5)
+            self.place_nodes[place] = node_id
+
+        # Transitions
+        for t in self.pn.transitions:
+            node_id = f"t_{t}"
+            color = '#7CFC00' if t in self.pn.fireable() else '#A9A9A9'
+            self.net.add_node(
+                node_id,
+                label=t,
+                shape='box',
+                color=color,
+                width = 40,
+                height = 40,
+                title="Click to fire"
+            )
+            self.transition_nodes[t] = node_id
+
+        # Edges
+        for t, arcs in self.pn.arcs_in.items():
+            for place, w in arcs:
+                self.net.add_edge(self.place_nodes[place], self.transition_nodes[t], label=str(w), smooth='curvedCW')
+        for t, arcs in self.pn.arcs_out.items():
+            for place, w in arcs:
+                self.net.add_edge(self.transition_nodes[t], self.place_nodes[place], label=str(w), smooth='curvedCCW')
+
+        self.built = True
+
+    def update_graph(self):
+        # Update places
+        for place, tokens in self.pn.places.items():
+            node_id = self.place_nodes[place]
+            self.net.get_node(node_id)['label'] = f"{place}\nTokens: {tokens}"
+            self.net.get_node(node_id)['size'] = 30 + tokens*5
+
+        # Update transitions
+        for t in self.pn.transitions:
+            node_id = self.transition_nodes[t]
+            color = '#7CFC00' if t in self.pn.fireable() else '#A9A9A9'
+            self.net.get_node(node_id)['color'] = color
+            self.net.get_node(node_id)['width'] = 40
+            self.net.get_node(node_id)['height'] = 40
+
+    def save_html(self, filename="petri_net.html"):
+        self.build_graph()
+        self.update_graph()
+        # Inject custom JS for clickable transitions
+        js = """
+        <script type="text/javascript">
+        network.on("click", function(params) {
+            if(params.nodes.length > 0) {
+                var nodeId = params.nodes[0];
+                if(nodeId.startsWith("t_")){
+                    fetch("/fire_transition", {
+                        method: "POST",
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify({transition: nodeId.substring(2)})
+                    }).then(response => response.json()).then(data => {
+                        // update node labels and colors
+                        for(const [id, label, color, size] of data.nodes){
+                            network.body.data.nodes.update({id:id,label:label,color:color,size:size});
+                        }
+                    });
+                }
+            }
+        });
+        </script>
+        """
+        self.net.save_graph(filename)
+        with open(filename, "a") as f:
+            f.write(js)
+
+# --- Flask Server ---
+app = Flask(__name__)
+pn = PetriNet()
+viz = PetriNetVisualizer(pn)
+
+@app.route("/")
+def index():
+    return send_file("petri_net.html")
+
+@app.route("/fire_transition", methods=["POST"])
+def fire_transition():
+    data = request.get_json()
+    transition = data.get("transition")
+    pn.fire(transition)
+    viz.update_graph()
+    # Send back updated node info: [id, label, color, size]
+    nodes_data = []
+    for place, tokens in pn.places.items():
+        node_id = viz.place_nodes[place]
+        label = f"{place}\nTokens: {tokens}"
+        size = 30 + tokens*5
+        nodes_data.append([node_id, label, '#89CFF0', size])
+    for t in pn.transitions:
+        node_id = viz.transition_nodes[t]
+        color = '#7CFC00' if t in pn.fireable() else '#A9A9A9'
+        nodes_data.append([node_id, t, color, 25])
+    return jsonify({"nodes": nodes_data})
+
+# --- Run Server ---
+def open_browser():
+    webbrowser.open("http://127.0.0.1:5000")
+
+if __name__ == "__main__":
+    viz.save_html("petri_net.html")
+    threading.Timer(1, open_browser).start()
+    app.run(debug=True)
